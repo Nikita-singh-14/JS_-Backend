c78dd9fe295bd610649dc74c68c870f0ab94da95
Nikita-singh-14 <nk2371174@gmail.com>
2026-08-14 06:42:40 +0530

    add getWatchHistory controller and update user routes to include it

--- 01_Backend/src/controllers/register.controller.js
+++ 01_Backend/src/controllers/register.controller.js
@@ -5,6 +5,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 const generateRefreshAndAccessTokens = async(userId) => {
     try {
@@ -422,6 +423,56 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
     )
 })
 
+const getWatchHistory = asyncHandler(async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $watch: {
+                _id: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id)",
+                as: "watchHistory",
+                pipeline: ([{
+                    $lookup: {
+                        from: "users",
+                        localField: "owner",
+                        foreignField: "_id",
+                        as: "owner",
+                        pipeline:([
+                            {
+                                $project: {
+                                    username: 1,
+                                    email: 1,
+                                    avatar: 1,
+                                }
+                            }
+                        ])
+                    },
+                    pipeline: ([
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ])
+                }]),
+                
+            }
+        }
+    ])
+
+    return res.status(200)
+    .json(new ApiResponse(
+        200, user[0].watchHistory, "Watch history fetched successfully"
+    ))
+})
+
 
 
 
@@ -434,5 +485,7 @@ export {
     getCurrentUser, 
     updateDetails, 
     updateUserAvatar,
-    getUserChannelProfile
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 }
--- 01_Backend/src/routes/user.routes.js
+++ 01_Backend/src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/register.controller.js";
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changePassword, 
+    getCurrentUser, 
+    updateDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/register.controller.js";
 import {upload} from "../middleware/multer.middleware.js"
 import { jwtVerify } from "../middleware/auth.middleware.js";
 const router = Router()
@@ -22,5 +34,13 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(jwtVerify, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(jwtVerify, changePassword)
+router.route("current-user").get(jwtVerify, getCurrentUser)
+router.route("update-details").patch(jwtVerify, updateDetails)
 
+router.route("avatar").patch(jwtVerify, upload.single("/avatar"), updateUserAvatar)
+router.route("cover-image").patch(jwtVerify, upload.single("/coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(jwtVerify, getUserChannelProfile)
+router.route("watch-history").get(jwtVerify, getWatchHistory)
 export default router
