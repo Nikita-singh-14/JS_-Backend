5ce88a370d50717a1a3ec26b369d22e8911a7534
Nikita-singh-14 <nk2371174@gmail.com>
2026-09-02 08:00:15 +0530

    chore: comment out dotenv/config import in index.js

--- 01_Backend/index.js
+++ 01_Backend/index.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import {DB_NAME} from './constants.js';
 import connectDB from './src/db/index.js';
-import "dotenv/config";
+//01-sept 2026
+// import "dotenv/config";
 import app from './app.js'
 
 dotenv.config({
