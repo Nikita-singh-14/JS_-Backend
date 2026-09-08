64b819b754d1073304d49a53f94c900def48d383
Nikita-singh-14 <nk2371174@gmail.com>
2026-09-08 07:29:37 +0530

    feat: update Logo component to display logo image

--- 01_Frontend/src/components/Logo.jsx
+++ 01_Frontend/src/components/Logo.jsx
@@ -1,7 +1,10 @@
 
 const Logo = () => {
   return (
-    <div>Logo</div>
+    <div>
+      <img src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png" alt="logo"
+            className='w-30' />
+    </div>
   )
 }
 
